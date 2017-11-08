5c04296fee51bf75c76b0be3b3e976fe400005ae
burceqian <33027444+burceqian@users.noreply.github.com>
2017-11-08 16:28:42 +0800

    Daily update

---
+++ Src/BQ3DSCommonFunction/BQIO.cs
@@ -0,0 +1,47 @@
+﻿using BQStructure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BQ3DSCommonFunction
+{
+    public class BQIO
+    {
+        private static string RomFolder = System.Environment.CurrentDirectory + @"\Roms\";
+        public static List<FileInfo> GetAllFilesInDirectory(DirectoryInfo directoryInfo, List<FileInfo> fileList)
+        {
+            //遍历文件
+            foreach (FileInfo file in directoryInfo.GetFiles())
+            {
+                fileList.Add(file);
+            }
+
+            //遍历文件夹
+            foreach (DirectoryInfo subfolder in directoryInfo.GetDirectories())
+            {
+                fileList = GetAllFilesInDirectory(subfolder, fileList);
+            }
+
+            return fileList;
+        }
+
+        public static void CopyRomToRomFolder(RomInfo romInfo, FileInfo file)
+        {
+            string lromFolder = RomFolder + romInfo.SubSerial + @"\";
+            string lromfile = lromFolder + romInfo.SubSerial + file.Extension;
+            FileInfo ltarFile = new FileInfo(lromfile);
+            if (Directory.Exists(lromFolder) == false)
+            {
+                Directory.CreateDirectory(lromFolder);
+            }
+
+            if (ltarFile.Exists == false)
+            {
+                File.Copy(file.FullName, ltarFile.FullName);
+            }
+        }
+    }
+}
